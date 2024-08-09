321b338effe78a665fb023c6584aecb9d4b7fb94
kamalsingh102 <92178317+kamalsingh102@users.noreply.github.com>
2024-08-09 19:02:04 +0530

    Update Data cleaning.sql

--- World layoffs/Data cleaning.sql
+++ World layoffs/Data cleaning.sql
@@ -3,7 +3,7 @@ FROM layoffs;
 
 
 
--- first thing we want to do is create a staging table. This is the one we will work in and clean the data. We want a table with the raw data in case something happens
+-- first thing we want to do is create a staging table. This is the one we will work in and clean the data. 
 CREATE TABLE world_layoffs.layoffs_staging 
 LIKE layoffs;
 
